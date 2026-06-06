7ba04391a2f50e291ebc8e12ca7e20ab2fb3b6b2
Sujan Sarkar <sujansarkar@Sujans-MacBook-Air.local>
2026-06-06 13:22:58 +0300

    Version 1: Created MAUI project structure

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordResetBruteForce_GUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PasswordResetBruteForce_GUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
